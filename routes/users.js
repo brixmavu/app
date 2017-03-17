dd3cb0dae9aa0745a3163f29981fca2c8b1ed814
brixton <brixton.mavu@rockectseed.com>
2017-03-17 10:46:45 +0200

    making mongo db to intergrate and login handlebars

--- routes/users.js
+++ routes/users.js
@@ -25,4 +25,28 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', function (req, res) {  
+  res.render('users/login', {title: 'login'});
+});
+
+router.post('/login', function (req, res) {  
+  console.log(req)
+});
+
+router.get('/register', function (req, res) {  
+  res.render('users/register', {title: 'register'});
+});
+
+router.post('/register', function (req, res) {  
+  console.log(req)
+});
+
+router.get('/forgot_password', function (req, res) {  
+  res.render('users/forgot_password', {title: 'forgot_password'});
+});
+
+router.post('/forgot_password', function (req, res) {  
+  console.log(req)
+});
+
 module.exports = router;
